7592e27d5d148fda2eb12f8432c12720e2f56874
Paritosh Shah <paritosh@pocketgems.com>
2014-04-08 15:27:42 -0700

    support for scraping bowling stats

--- stats/scraper.js
+++ stats/scraper.js
@@ -18,6 +18,7 @@ function scrape_list_of_matches(page) {
 	});
 }
 
+// output format: [id, name, runs, balls, sixes]
 function scrape_batting_stats(page) {
 	return page.evaluate(function() {
 		var batting_stats = [];
@@ -25,43 +26,49 @@ function scrape_batting_stats(page) {
 			var player = $('a', this);
 			if (player.length == 0) return;
 			var player_id = player.attr("href").split('/').pop().replace(/\..*/g, '');
+			var name = player.text();
 			var player_data = $(this).text().trim().replace(/ /g,'').split('\n');
 			var runs = player_data[2], balls = player_data[4], sixes = player_data[6];
-			batting_stats.push([player_id, runs, balls, sixes]);
+			batting_stats.push([player_id, name, runs, balls, sixes]);
 		});
 		return batting_stats;
 	});
 }
-//
-//function scrape_bowling_points(page) {
-//	return page.evaluate(function() {
-//		var player_points = [];
-//		$('#inningsBowl1 > tbody .inningsRow, #inningsBowl2 > tbody .inningsRow').each(function() {
-//			var player = $('a', this);
-//			if (player.length == 0) return;
-//			var player_id = player.attr("href").split('/').pop().replace(/\..*/g, '');
-//			var player_data = $(this).text().trim().replace(/ /g,'').split('\n');
-//			var o = player_data[1], maidens = player_data[2], runs = player_data[3], wickets = player_data[4];
-//			var overs = o.split(".");
-//			overs.push(0);
-//			var balls = overs[0] * 6 + overs[1];
-//
-//			var impact = runs === 0 ? -5 : 2 * sixes;
-//			var points = 20 * wickets + balls * 1.5 - runs + 10 * runs / 25 + impact;
-//
-//			player_points.push([player_id, points])
-//		});
-//		return player_points;
-//	});
-//}
-//
+
+function scrape_bowling_stats(page) {
+	return page.evaluate(function() {
+		var bowling_stats = [];
+		$('#inningsBowl1 > tbody .inningsRow, #inningsBowl2 > tbody .inningsRow').each(function() {
+			var player = $('a', this);
+			if (player.length == 0) return;
+			var player_id = player.attr("href").split('/').pop().replace(/\..*/g, '');
+			var name = player.text();
+			var player_data = $(this).text().trim().replace(/ /g,'').split('\n');
+			var o = player_data[1], maidens = player_data[2], runs = player_data[3], wickets = player_data[4];
+			var overs = o.split(".");
+			overs.push("0");
+			var balls = parseInt(overs[0]) * 6 + parseInt(overs[1]);
+
+			bowling_stats.push([player_id, name, wickets, balls, runs, maidens]);
+		});
+		return bowling_stats;
+	});
+}
+
 
 //url = 'http://stats.espncricinfo.com/indian-premier-league-2013/engine/records/team/match_results.html?id=2013;trophy=117;type=season';
 //fetch(url, function(page) { console.log(scrape_list_of_matches(page)); phantom.exit(); });
 
+//url = 'http://www.espncricinfo.com/indian-premier-league-2013/engine/match/597998.html';
+//fetch(url, function(page) {
+//	stats = scrape_batting_stats(page);
+//	for(i=0; i<stats.length; i++) console.log(stats[i]); 
+//	phantom.exit(); 
+//});
+
 url = 'http://www.espncricinfo.com/indian-premier-league-2013/engine/match/597998.html';
 fetch(url, function(page) {
-	stats = scrape_batting_stats(page);
+	stats = scrape_bowling_stats(page);
 	for(i=0; i<stats.length; i++) console.log(stats[i]); 
 	phantom.exit(); 
 });
