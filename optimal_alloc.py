6138beb722ed814490fc7bc3e69909cbef6fcdd4
Paritosh Shah <paritosh@pocketgems.com>
2014-04-07 18:49:17 -0700

    final set of changes from last year

--- optimal_alloc.py
+++ optimal_alloc.py
@@ -16,9 +16,12 @@ teams_list = ['CSK','DD','KKR','KXIP','MI','PWI','RCB','RR','SH']
 s = []
 top_picks = [[] for i in range(9)]
 
+blacklist = ["Manpreet Gony", "Dhawal Kulkarni", "Harmeet Singh Bansal",
+"Venugopal Rao", "Anand Rajan"]
+
 def fetch_data(url):
     http = httplib2.Http(disable_ssl_certificate_validation=True)
-    headers = { 'Cookie': 'session_id_ifl="966:4c650ae2-0b1b-4e77-ac1b-4c6222b0e136"' }
+    headers = { 'Cookie': 'session_id_ifl="890:fd25193b-c841-409c-933f-53385da34231"' }
     return json.loads(http.request(url, 'GET', headers=headers)[1])
 
 all_players = fetch_data('https://fantasy.iplt20.com/ifl/research/get_res_data_list.json?json')["players"]
@@ -36,8 +39,10 @@ for i in range(len(all_players)):
     p = all_players[i]
     if p["vfm"] == None:
         p["point_avg"] = 0
+        p["weight"] = 0
     else:
         p["point_avg"] = p["vfm"] * p["price"] / vfm_factor
+        p["weight"] = float(str(p["point_avg"]) + "." + str(p["id"]))
 
     p["team_id"] = teams[p["team_name"]]
     player_info[p["id"]] = p
@@ -48,6 +53,7 @@ with open('allocation') as f:
         player_name = data[i].strip()
         player = [p for p in all_players if p["firstname"] == player_name][0]
         fanta_team.append(player["id"])
+        print player["firstname"], player["point_avg"]
         t = player["team_name"]
         ga[teams[t]].append(player)
         
@@ -63,13 +69,13 @@ def top_players(players, n):
     return sorted(players, key=lambda(x): -1 * x["point_avg"])[:n]
 
 def top_picks_for_team(t):
-    players = [p for p in all_players if p["team_name"] == teams_list[t] and p["id"] not in fanta_team]
+    players = [p for p in all_players if p["team_name"] == teams_list[t] and
+            p["id"] not in fanta_team and p["firstname"] not in blacklist]
     return top_players(players, 5)
 
 for i in range(9):
     top_picks[i] = top_picks_for_team(i)
 
-#print [ [p["firstname"], p["point_avg"]] for p in top_players([ p for p in all_players if p["country_id"] == 2 ], 15) ]
 
 #print [ [p["firstname"], p["point_avg"]] for p in top_players([ p for p in all_players if p["country_id"] == 1 and p["skill_id"] == 4 ], 15) ]
 
@@ -97,8 +103,9 @@ def is_valid(t):
     keepers = len( [p for p in team if p["skill_id"] == 1])
     bowlers = len([p for p in team if p["skill_id"] == 3])
     overseas = len([p for p in team if p["country_id"] == 2])
+    cost = sum([ p["price"] for p in team ])
 
-    return batsmen > 3 and batsmen < 6 and all_rounders > 0 and all_rounders < 5 and keepers == 1 and bowlers > 1 and bowlers < 6 and overseas < 5
+    return batsmen > 3 and batsmen < 6 and all_rounders > 0 and all_rounders < 5 and keepers == 1 and bowlers > 1 and bowlers < 6 and overseas < 5 and cost < 10000001
 
 def base_potential(t, s, k):
     return accum_ids( [p for p in t if player_info[p]["team_id"] in [s[k][0], s[k][1]]] )
@@ -153,10 +160,154 @@ def potential(t, k):
 
     return max_potential, sub_out, sub_in
 
-#print potential(sorted(fanta_team), 0)
-#print base_potential(sorted(fanta_team), s, 0)
-#print base_potential(sorted(fanta_team), s, 1)
-#for t in s[0]:
-#    print teams_list[t]
-#    print [ [p["firstname"], p["point_avg"]] for p in top_picks_for_team(t) ]
+# given a team t, and a bench b, return the next worse (t', b')
+def next_worse_pick(t, bench, err):
+    i = 0
+    j = 0
 
+    min_delta = 1000
+    sub_out = 0
+    sub_in = 0
+
+
+    b = list(bench)
+    if err == "TOO_FEW_BATSMEN":
+        b = [p for p in bench if p["skill_id"] == 4]
+    elif err == "NO_ALL_ROUNDERS":
+        b = [p for p in bench if p["skill_id"] == 2]
+    elif err == "NEED_A_KEEPER":
+        b = [p for p in bench if p["skill_id"] == 1]
+    elif err == "NOT_ENOUGH_BOWLERS":
+        b = [p for p in bench if p["skill_id"] == 3]
+    elif err == "NO_UNCAPPED":
+        b = [p for p in bench if p["capped"] == 0]
+    elif err == "NO_ALL_ROUNDERS":
+        b = [p for p in bench if p["skill_id"] == 2]
+
+    batsmen = len([p for p in t if p["skill_id"] == 4])
+    all_rounders = len([p for p in t if p["skill_id"] == 2])
+    keepers = len( [p for p in t if p["skill_id"] == 1])
+    bowlers = len([p for p in t if p["skill_id"] == 3])
+    overseas = len([p for p in t if p["country_id"] == 2])
+    uncapped = len([p for p in t if p["capped"] == 0])
+ 
+    for i in range(len(t)):
+        for j in range(len(b)):
+            delta = t[i]["weight"] - b[j]["weight"]
+
+            making_things_worse = False
+            
+            if err == "TOO_FEW_BATSMEN":
+                making_things_worse = (t[i]["skill_id"] == 4)
+            elif err == "TOO_MANY_BATSMEN":
+                making_things_worse = (t[i]["skill_id"] != 4)
+            elif err == "TOO_MANY_ALL_ROUNDERS":
+                making_things_worse = (t[i]["skill_id"] != 2)
+            elif err == "TOO_MANY_KEEPERS":
+                making_things_worse = (t[i]["skill_id"] != 1)
+            elif err == "NOT_ENOUGH_BOWLERS":
+                making_things_worse = (t[i]["skill_id"] == 3)
+            elif err == "TOO_MANY_BOWLERS":
+                making_things_worse = (t[i]["skill_id"] != 3)
+            elif err == "TOO_MANY_OVERSEAS":
+                making_things_worse = (t[i]["country_id"] != 2)
+ 
+            if not making_things_worse and delta > 0 and delta < min_delta:
+                min_delta = delta
+                sub_out = i
+                sub_in = j
+
+    player_out = t[sub_out]
+    player_in = b[sub_in]
+
+    i = 0
+    del t[sub_out]
+    while i < len(t) and t[i]["weight"] > player_in["weight"]:
+        i = i + 1
+    t.insert(i, player_in)
+
+
+    for i in range(len(bench)):
+        if bench[i]["id"] == player_in["id"]:
+            break
+
+    del bench[i]
+
+    i = 0
+    while i < len(bench) and bench[i]["weight"] > player_out["weight"]:
+        i = i + 1
+    bench.insert(i, player_out)
+
+    return t, bench
+
+def check_for_errors(team, teams_playing):
+    batsmen = len([p for p in team if p["skill_id"] == 4])
+    all_rounders = len([p for p in team if p["skill_id"] == 2])
+    keepers = len( [p for p in team if p["skill_id"] == 1])
+    bowlers = len([p for p in team if p["skill_id"] == 3])
+    overseas = len([p for p in team if p["country_id"] == 2])
+    uncapped = len([p for p in team if p["capped"] == 0])
+    players_from_each_team = [len([p for p in team if p["team_name"] ==
+            teams_list[t]]) for t in teams_playing]
+    cost = sum([ p["price"] for p in team ])
+    
+    if batsmen < 4:
+        return "TOO_FEW_BATSMEN"
+    elif batsmen > 5:
+        return "TOO_MANY_BATSMEN"
+    elif all_rounders == 0:
+        return "NO_ALL_ROUNDERS"
+    elif all_rounders > 4:
+        return "TOO_MANY_ALL_ROUNDERS"
+    elif keepers == 0:
+        return "NEED_A_KEEPER"
+    elif keepers > 1:
+        return "TOO_MANY_KEEPERS"
+    elif bowlers < 2:
+        return "NOT_ENOUGH_BOWLERS"
+    elif bowlers > 5:
+        return "TOO_MANY_BOWLERS"
+    elif uncapped == 0:
+        return "NO_UNCAPPED"
+    elif max(players_from_each_team) > 6:
+        return "TOO_MANY_FROM_ONE_TEAM"
+    elif cost > 10000000:
+        return "TOO_COSTLY"
+    elif overseas > 4:
+        return "TOO_MANY_OVERSEAS"
+
+    return None
+
+
+import time
+
+def daily_challenge(teams_playing):
+    pool = [p for p in all_players if teams[p["team_name"]] in teams_playing and p["point_avg"] > 0 and p["firstname"] not in blacklist]
+    pool = sorted(pool, key=lambda(x): -1 * x["weight"])
+
+    t = pool[:11]
+
+    print [[p["firstname"], p["point_avg"]] for p in t]
+
+    b = pool[11:]
+
+    err = check_for_errors(t, teams_playing)
+
+    while err != None:
+        t, b = next_worse_pick(t, b, err)
+        err = check_for_errors(t, teams_playing)
+        print sum([ p["point_avg"] for p in t]), err
+
+    return t
+
+
+#print [[p["firstname"], p["point_avg"]] for p in daily_challenge(s[0])]
+print potential(sorted(fanta_team), 0)
+#print base_potential(sorted(fanta_team), s, 0)
+for t in s[0]:
+    print teams_list[t]
+    print [ [p["firstname"], p["point_avg"]] for p in top_picks_for_team(t) ]
+
+#print [ [p["firstname"], p["point_avg"]] for p in top_players([ p for p in all_players if p["country_id"] == 2 ], 15) ]
+
+#print [ [p["firstname"], p["point_avg"]] for p in top_players([ p for p in all_players if p["country_id"] == 1 ], 10) ]
