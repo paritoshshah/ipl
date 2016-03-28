42469760dce433237c3694c6a2a6758c65cc7d2f
Paritosh Shah <paritosh@pocketgems.com>
2016-03-28 16:52:35 -0700

    injested 2014 raw json stats into db

--- stats/fantasy.iplt20/json_to_db.py
+++ stats/fantasy.iplt20/json_to_db.py
@@ -3,10 +3,10 @@
 import json
 import sqlite3
 
-all_players = json.load(open('raw/2015/2015_get_res_data_list.json'))["players"]
+all_players = json.load(open('raw/2014/player_researchall.json'))["players"]
 
 # create table player_match_stats( player_id INT, match_id INT, opponent_id INT, opponent CHAR(50), batting_pts REAL, bowling_pts REAL, fielding_pts REAL, bonus_pts REAL, total_pts REAL )
-conn = sqlite3.connect('player_stats_2015.db')
+conn = sqlite3.connect('player_stats_2014.db')
 def persist_player_stats(stats):
     c = conn.cursor()
     data = [stats['id'], stats['firstname'], stats['bat_points'], stats['bol_points'], stats['field_points'], stats['bonus_points'], stats['fanta_points']]
