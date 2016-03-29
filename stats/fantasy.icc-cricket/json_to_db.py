06dd50c094d3030cf086cac651b9e5ba71a2b7e7
Paritosh Shah <paritosh@pocketgems.com>
2016-03-29 11:50:31 -0700

    merged in 2016 wt20 stats

---
+++ stats/fantasy.icc-cricket/json_to_db.py
@@ -0,0 +1,20 @@
+# parse fantasy.iplt20.com raw json stats to sqlite3 db
+
+import json
+import sqlite3
+
+# create table player_match_stats( player_id INT, match_id INT, opponent_id INT, opponent CHAR(50), batting_pts REAL, bowling_pts REAL, fielding_pts REAL, bonus_pts REAL, total_pts REAL )
+conn = sqlite3.connect('player_stats.db')
+def persist_player_stats(stats):
+    c = conn.cursor()
+    data = [stats['playerName'], stats['score']]
+    c.execute("insert into player_stats_2016_raw values(?,?)", data)
+    conn.commit()
+    c.close()
+
+
+for i in range(13, 33):
+    all_players = json.load(open('raw/2016/' + str(i) + '.json'))
+    for p in all_players:
+        if p["matchCount"] == 1:
+            persist_player_stats(p)
